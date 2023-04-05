350d09002f72b983cd9559ea2bc12a69f2e3e850
s p <sp@ss-MacBook-Air.local>
2023-04-05 15:13:42 -0400

    better answer

--- script.js
+++ script.js
@@ -5,22 +5,14 @@ container.style.width = '800px'
 container.style.height = '800px'
 container.style.backgroundColor = 'red'
 
-for (let i = 1; i <= 64; i++) {
+for (let i = 0; i <= 64; i++) {
     const tile = document.createElement('div')
     tile.style.width = '12.5%'
     tile.style.height = '12.5%'
-    if (Math.floor((i - 1) / 8) % 2 === 0) {
-        if (i % 2 === 1) {
-            tile.style.backgroundColor = 'black'
-        } else {
-            tile.style.backgroundColor = 'red'
-        }
+    if ((i + Math.floor(i / 8)) % 2 === 0) {
+        tile.style.backgroundColor = 'red'
     } else {
-        if (i % 2 === 1) {
-            tile.style.backgroundColor = 'red'
-        } else {
-            tile.style.backgroundColor = 'black'
-        }
+        tile.style.backgroundColor = 'black'
     }
 
     container.append(tile)
